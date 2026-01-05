375ae4cbaa8525fb9724c3385ccc94d07a40ae51
shayan-khan10 <shayannn.khan.10@gmail.com>
2026-01-05 19:17:39 +0500

    fixed tree hydration error from ModeToggle

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -14,6 +14,15 @@ import {
 
 export function ModeToggle() {
   const { setTheme } = useTheme()
+  const [mounted, setMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
 
   return (
     <DropdownMenu>
